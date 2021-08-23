59192e881708b1dcfdd46c56759e921a6cf21b22
bakachan <63496788+bakachanbaby@users.noreply.github.com>
2021-08-23 20:57:49 +0700

    Initial commit

---
+++ js.js
@@ -0,0 +1,7 @@
+// - Attribute - Thuộc tính: là sử dụng thuộc tính 
+//Inline -Block
+//Những thẻ Inline thường dùng
+//+ a, b, br, input, button, label, span
+
+//Những thẻ block thường dùng
+//+H1->h6, dive, ul, li, p, table, 
